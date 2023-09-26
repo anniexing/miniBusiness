2e0d1ee3ba70fc85bf5dca724264c82a76a52ba5
Annie Xing <mxxing@live.com>
2023-09-26 12:03:37 +0200

    implement marketing

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  if(el) {
+    const root = createRoot(el);
+    root.render(<App/>);
+  }
+
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.getElementById('_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
